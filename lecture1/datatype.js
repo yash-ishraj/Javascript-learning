2723cd686449036816659076e06981b885b17c6c
yash-ishraj <as7488896@gmail.com>
2024-10-25 14:28:32 +0000

    Datatype done

---
+++ lecture1/datatype.js
@@ -0,0 +1,16 @@
+"use strict";//Used for writing modern day javascript
+// alert(3+3)//alert can not be executed because it is not web browser it is node.js
+//console.log(3+3) console.log("ashish");//Not readable because of the lack of new line
+console.log(3+3);
+console.log("ashish")
+let username ="Ashish raj"
+let password="ashishjee123@"
+let isloggedin= true
+console.log(isloggedin)
+console.log(typeof(null))
+//number:0 to 2pow53
+//string=""
+//boolean true/false
+//null=>standalone
+//undefined
+//symbol=>unique
--- lecture1/test.js
+++ lecture1/test.js
@@ -1,1 +1,13 @@
-console.log("hiii");
+console.log("hiii")
+const accountId="991023"
+let username = "Ashish"
+let password="28719"
+city="ravangla"
+let consent;
+/*
+Var has scope problem hence it is not used nowadays
+*/
+/*
+const variable's can not be reinitialized
+*/
+console.table([accountId, username,password,city,consent]);
